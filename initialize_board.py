42e5f37a978747f0e8e46a0a81938adf11da6cf9
czhu21 <54689225+czhu21@users.noreply.github.com>
2019-10-24 18:20:58 -0400

    Add files via upload

--- initialize_board.py
+++ initialize_board.py
@@ -3,6 +3,7 @@
 # Group 5: Aditiya Suru, Casey Zhu, Lincoln Kartchner
 # initialize_board.py
 
+import sys
 
 def read_board(filename):
     '''
@@ -16,83 +17,102 @@ def read_board(filename):
 
     f = open(filename, "r")
     valid_grid = ['o', 'x', 'a', 'b', 'c']
-    spacial_blocks = ['a', 'b', 'c']
+    special_blocks = {
+        'a': 'reflect',
+        'b': 'opaque',
+        'c': 'refract'
+    }
+
     grid = []
-
+    start = False
     for i in f.readlines():
-        if i == 'GRID START':
-            break
-        else:
-            continue
-
-    for i in f.readlines():
-        if i == 'GRID STOP':
-            break
-        else:
+        if start:
+            if "GRID STOP" in i:
+                break
             grid.append(i)
-            continue
+        else:
+            if "GRID START" in i:
+                start = True
 
     f.seek(0)
+    start = False
     for i in f.readlines():
-        if i[0] == "#" or i[0] == '\n':
-            continue
+        if start:
+            if i[0] == "#" or i[0] == '\n':
+                continue
 
-        # elif i[0].lower() in valid_grid:
-        #     grid.append(i.strip())
+            elif i[0].upper() == 'A':
+                line = ''.join(i.split())
+                count = int(line[1:])
+                for i in range(count):
+                    pass
+                    # Initialize reflect block
 
-        elif i[0].upper() == 'A':
-            line = ''.join(i.split())
-            count = int(line[1:])
-            for i in range(count):
-                pass
-                # Initialize reflect block
+            elif i[0].upper() == 'B':
+                line = ''.join(i.split())
+                count = int(line[1:])
+                for i in range(count):
+                    pass
+                    # Initialize opaque block
 
-        elif i[0].upper() == 'B':
-            line = ''.join(i.split())
-            count = int(line[1:])
-            for i in range(count):
-                pass
-                # Initialize opaque block
+            elif i[0].upper() == 'C':
+                line = ''.join(i.split())
+                count = int(line[1:])
+                for i in range(count):
+                    pass
+                    # Initialize refract block
 
-        elif i[0].upper() == 'C':
-            line = ''.join(i.split())
-            count = int(line[1:])
-            for i in range(count):
-                pass
-                # Initialize refract block
+            elif i[0].upper() == 'L':
+                line = i.split()
+                # Initialize lazor
+                x = int(line[1])
+                y = int(line[2])
+                vx = int(line[3])
+                vy = int(line[4])
 
-        elif i[0].upper() == 'L':
-            line = i.split()
-            # Initialize lazor
-            x = int(line[1])
-            y = int(line[2])
-            vx = int(line[3])
-            vy = int(line[4])
+            elif i[0].upper() == 'P':
+                line = i.split()
+                # Initialize target
+                x = int(line[1])
+                y = int(line[2])
 
-        elif i[0].upper() == 'P':
-            line = i.split()
-            # Initialize target
-            x = int(line[1])
-            y = int(line[2])
-
+            else:
+                #print('Warning: line begins with an invalid character')
+                continue
         else:
-            #print('Warning: line begins with an invalid character')
-            continue
+            if "GRID STOP" in i:
+                start = True
 
     for i, v in enumerate(grid):
         grid[i] = v.split(' ')
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            grid[i][j] = grid[i][j].strip()
+            grid[i][j] = grid[i][j].lower()
+
+            if grid[i][j] in valid_grid:
+                pass
+            else:
+                print('Error: Invalid block in grid!')
+                sys.exit()
 
     NS = []
     EW = []
 
-    # for i in range(len(grid)):
-    #     for j in range(len(grid[0])):
-    #         print(grid[i][j])
-
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] in special_blocks:
+                NS.append({(2*i+1, 2*j): special_blocks[grid[i][j]]})
+                NS.append({(2*i+1, 2*j+2): special_blocks[grid[i][j]]})
+                EW.append({(2*i, 2*j+1): special_blocks[grid[i][j]]})
+                EW.append({(2*i+2, 2*j+2): special_blocks[grid[i][j]]})
+    print(NS)
+    print(EW)
     return(grid)
     f.close()
 
 
 if __name__ == "__main__":
-    x = read_board('mad_4.bff')
+    x = read_board('yarn_5.bff')
     print(x)
